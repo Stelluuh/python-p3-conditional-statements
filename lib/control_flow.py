0a8b7d0983d5a1331c5bd12824b75ba488b87f02
stella <sftamesis13@gmail.com>
2024-04-23 20:50:02 -0700

    pass 1

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+# Write a function admin_login() that takes two arguments, a username and a password. If the username is "admin" or "ADMIN" and the password is "12345", return "Access granted". Otherwise, return "Access denied".
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.upper() == "ADMIN" and password == "12345":
+        return "Access granted"
+    
+    return "Access denied"
+    
 
+# Write a function hows_the_weather() that takes in one argument, a temperature. If the temperature is below 40, return "It's brisk out there!". If the temperature is between 40 and 65, return "It's a little chilly out there!". If the temperature is above 85, return "It's too dang hot out there!". Otherwise, return "It's perfect out there!"
 def hows_the_weather(temperature):
-    # your code here
     pass
 
 def fizzbuzz(num):
